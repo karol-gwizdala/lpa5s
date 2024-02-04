51fc29a903319580a53365e8255ca20bab792604
karol-gwizdala <gwizdala.karol@gmail.com>
2024-02-04 22:37:09 +0100

    add: navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "@picocss/pico";
+import { Navigation } from "./components/navigation";
+import { Home } from "./pages/home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navigation />
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+    </>
   );
-}
-
+};
 export default App;
---
+++ src/components/navigation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Navigation = () => {
+  return (
+    <nav className='container'>
+      <ul>
+        <li>LPA5S</li>
+      </ul>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+      </ul>
+    </nav>
+  )
+}
